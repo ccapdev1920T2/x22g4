c68576865197fb5f52b3f43da3098000f24ac06e
Justin <justingalura@gmail.com>
2020-04-14 21:34:57 +0800

    Cat Profile get Complete

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
+const exphbs = require('express-handlebars');
+var path = require('path');
 
 const routes = require('./routes/routes.js');
 
@@ -10,11 +12,30 @@ const port = 3000;
 
 app.set('view engine', 'hbs');
 
+app.engine('hbs', exphbs({
+	extname: 'hbs',
+	defaultView: 'main',
+	layoutsDir: path.join(__dirname, '/views/layouts'),
+	partialsDir: path.join(__dirname, '/views/partials'),
+
+	//Helpers
+	helpers: {
+		cap: function(text) { return text.toUpperCase(); },
+		em: function(text) {
+			var x = `<em>${text}</em>`;
+
+			return new handlebars.SafeString(x);
+		}
+	}
+}));
+
+app.set('view engine', 'hbs');
+
 hbs.registerPartials(__dirname + '/views/partials');
 
 app.use(express.urlencoded({extended: true}));
 
-var path = require('path');
+
 app.use(express.static(path.join(__dirname,'public')));
 
 app.use('/', routes);
