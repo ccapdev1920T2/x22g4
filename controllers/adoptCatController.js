617e952fb3cf55a934ed760b48627cb2cbed066c
Justin <justingalura@gmail.com>
2020-04-15 00:42:23 +0800

    Completed Adopt a Cat Page Connection

--- controllers/adoptCatController.js
+++ controllers/adoptCatController.js
@@ -2,17 +2,21 @@ const database = require("../models/database.js");
 
 const Cat = require("../models/cat.js");
 
+const LAGUNA_QUERY = {location: 'Laguna Campus'};
+const MANILA_QUERY = {location: 'Taft Campus'};
 const adoptCatController = {
 
     getCatCards: function (req, res) {
-        database.findMany(Cat, null, null, function(result) {
-            console.log(result);
-            res.render('adoptCat', {lagunaCats: result});
+        var lagunaResult;
+        var manilaResult;
+
+        database.findManyLean(Cat, LAGUNA_QUERY, null, function(lagunaResult) {
+            database.findManyLean(Cat, MANILA_QUERY, null, function(manilaResult) {
+                res.render('adoptCat', {lagunaCats: lagunaResult, manilaCats: manilaResult});
+            });
         });
 
-
     }
-
 }
 
 module.exports = adoptCatController;
