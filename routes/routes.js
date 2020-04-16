e9e029b428983fc7b7b0f672749bb65cafe5ecc7
Jacob Darvin <jacobisdarvin@gmail.com>
2020-04-16 15:01:46 +0800

    fixed routing

--- routes/routes.js
+++ routes/routes.js
@@ -8,6 +8,28 @@ const app = express();
 app.get("/cat/:name", catProfileController.getCatProfile);
 app.get("/adoptCat", adoptCatController.getCatCards);
 
+//Home Route
+app.get('/', function(req, res){
+    res.render('index', {
+        title: 'Home',
+        home_active: true,
+    })  
+});
 
+app.get('/home(page)?(.html)?', function(req, res) {
+    res.render('index', {
+        title: 'Home',
+        home_active: true,
+    })
+});
+
+//Donate-Feed
+app.get('/donate', function(req, res){
+    res.render('donate', {
+        title: 'Donate',
+        donate_active: true,
+    })  
+});
+//Donate-Feed
 
 module.exports = app;
